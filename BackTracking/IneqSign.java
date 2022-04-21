d2fd34abbf7e979f53d81c27e0a6c929da7738b6
SangMin90 <everminelee@naver.com>
2022-04-22 00:21:52 +0900

    Modified BackTracking - InequalitySign Algorithm Code push

--- BackTracking/IneqSign.java
+++ BackTracking/IneqSign.java
@@ -6,20 +6,23 @@ public class IneqSign {
     static boolean[] check = new boolean[10];
     static char[] ineq;
     static ArrayList<String> ans = new ArrayList<>();
-    static boolean ok(String num) {
-        for (int i=0;i<k;i++) {
-            if (ineq[i] == '<') if (num.charAt(i) > num.charAt(i+1)) return false;
-            if (ineq[i] == '>') if (num.charAt(i) < num.charAt(i+1)) return false;
-        }
+    static boolean keep(char a, char b, char op) {
+        if (op == '<') if (a > b) return false;
+        if (op == '>') if (a < b) return false;
         return true;
     }
     static void go(int index, String num) {
-        if (index == k+1) if (ok(num)) ans.add(num);
+        if (index == k+1) {
+            ans.add(num);
+            return;
+        }
         for (int i=0;i<10;i++) {
             if (check[i]) continue;
-            check[i] = true;
-            go(index+1,num+Integer.toString(i));
-            check[i] = false;
+            if (index == 0 || keep(num.charAt(index-1),(char)(i+'0'),ineq[index-1])) {
+                check[i] = true;
+                go(index + 1, num + i);
+                check[i] = false;
+            }
         }
     }
     public static void main(String[] args) {
