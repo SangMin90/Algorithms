633c54eac9628248dd68c3bd1edf7be45994a98b
SangMin90 <everminelee@naver.com>
2022-06-13 20:19:21 +0900

    DynamicProgramming - 카드 구매하기(bottmo-up) Code push

---
+++ DynamicProgramming/purchaseCardBotUp.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class purchaseCardBotUp {
+    public static void main(String[] args) {
+        int[] d;
+        int[] p;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        d = new int[n+1];
+        p = new int[n+1];
+        for (int i=1;i<=n;i++) {
+            p[i] = sc.nextInt();
+        }
+        for (int i=1;i<=n;i++) {
+            int temp = 0;
+            for (int j=1;j<=i;j++) {
+                temp = d[i-j]+p[j];
+                if (d[i] < temp) {
+                    d[i] = temp;
+                }
+            }
+        }
+        System.out.println(d[n]);
+    }
+}
