152ceb75521e7c2321a5f8d2bd0b60c4f01b4125
SangMin90 <everminelee@naver.com>
2022-06-13 21:18:55 +0900

    DynamicProgramming - 카드 구매하기2(up-down&bottom-up) Code push

---
+++ DynamicProgramming/purchaseCard2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class purchaseCard2 {
+    static int[] d;
+    static int[] p;
+    static int go(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (d[n] > 0) {
+            return d[n];
+        }
+        int temp = 0;
+        for (int i=1;i<=n;i++) {
+            temp = go(n-i) + p[i];
+            if (d[n] == -1 || d[n] > temp) {
+                d[n] = temp;
+            }
+        }
+        return d[n];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        d = new int[n+1];
+        for (int i=1;i<=n;i++) {
+            d[i] = -1;
+        }
+        p = new int[n+1];
+        for (int i=1;i<=n;i++) {
+            p[i] = sc.nextInt();
+        }
+        System.out.println(go(n));
+    }
+}
