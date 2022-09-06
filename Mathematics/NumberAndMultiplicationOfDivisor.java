3a1b8c381665f46c7699643a8e49b8aed12921fb
SangMin90 <everminelee@naver.com>
2022-09-07 01:27:36 +0900

    수학 - 약수의 개수와 덧셈 Code push

---
+++ Mathematics/NumberAndMultiplicationOfDivisor.java
@@ -0,0 +1,27 @@
+public class NumberAndMultiplicationOfDivisor {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for (int i=left;i<=right;i++) {
+            int numberOfDivisor = getNumberOfDivisor(i);
+            if (numberOfDivisor % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
+        return answer;
+    }
+    static int getNumberOfDivisor(int number) {
+        int count = 0;
+        for (int divisor=1;divisor*divisor<=number;divisor++) {
+            if (number % divisor == 0) {
+                if (divisor*divisor == number) {
+                    count++;
+                } else {
+                    count += 2;
+                }
+            }
+        }
+        return count;
+    }
+}
