f7bc94d190f99b6e542f00f719502a2ea7f9e2d4
SangMin90 <everminelee@naver.com>
2022-09-14 05:14:43 +0900

    수학 - 콜라츠 추측 Code push

---
+++ Mathematics/CollatzConjecture.java
@@ -0,0 +1,22 @@
+public class CollatzConjecture {
+    public static void main(String[] args) {
+        CollatzConjecture sample = new CollatzConjecture();
+        System.out.println(sample.solution(1));
+    }
+
+    public int solution(int num) {
+        int cnt = 0;
+        long temp = num;
+        while (temp != 1) {
+            if (temp % 2 == 0) {
+                temp /= 2;
+            } else {
+                temp *= 3;
+                temp++;
+            }
+            cnt++;
+        }
+        int answer = cnt <= 500 ? cnt : -1;
+        return answer;
+    }
+}
