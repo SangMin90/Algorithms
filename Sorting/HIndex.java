ee9254ff940b4d25d05b275e3a2f6c7dac0bbb2f
SangMin90 <everminelee@naver.com>
2022-10-31 20:00:45 +0900

    정렬 - H-Index Code push

---
+++ Sorting/HIndex.java
@@ -0,0 +1,48 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class HIndex {
+    public static void main(String[] args) {
+        HIndex sample = new HIndex();
+        System.out.println(sample.solution(new int[]{10, 8, 5, 4, 3}));
+    }
+    public int solution(int[] citations) {
+        int answer = findHIndex(citations);
+
+        return answer;
+    }
+    private int findHIndex(int[] c) {
+        Arrays.sort(c);
+
+        int[] cited = judgeCited(c);
+
+        return getHIndex(c, cited);
+    }
+
+    private int getHIndex(int[] c, int[] cited) {
+        int x = c.length + 1;
+        int y = 0;
+
+        for (int i = c[c.length - 1]; i >= 0; i--) {
+            if (cited[i] > 0) {
+                x -= cited[i];
+
+                y += cited[i];
+            }
+            if (i >= x && i <= y) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private int[] judgeCited(int[] c) {
+        int[] cited = new int[10001];
+
+        for (int i : c) {
+            cited[i] += 1;
+        }
+
+        return cited;
+    }
+}
