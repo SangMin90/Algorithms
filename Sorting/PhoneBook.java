ff5bb34dcd544fb498a7d176681a8135328ced7e
SangMin90 <everminelee@naver.com>
2022-11-04 20:13:55 +0900

    정렬 - 전화번호 목록 Code push

---
+++ Sorting/PhoneBook.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class PhoneBook {
+    public static void main(String[] args) {
+        PhoneBook sample = new PhoneBook();
+        System.out.println(sample.solution(new String[]{"119", "97674223", "1195524421"}));
+    }
+
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        return existPrefixPhoneNumber(phone_book);
+    }
+
+    private boolean existPrefixPhoneNumber(String[] phone_book) {
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i].length() < phone_book[i + 1].length() && phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
