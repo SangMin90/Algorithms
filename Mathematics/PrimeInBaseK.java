1959b0619b4d4cb5a341529822a7c29908ee6cba
SangMin90 <everminelee@naver.com>
2022-11-04 22:29:57 +0900

    k진수에서 소수 개수 구하기 Code push

---
+++ Mathematics/PrimeInBaseK.java
@@ -0,0 +1,35 @@
+public class PrimeInBaseK {
+    public static void main(String[] args) {
+        PrimeInBaseK sample = new PrimeInBaseK();
+        System.out.println(sample.solution(56506, 3));
+    }
+    public int solution(int n, int k) {
+        String s = Long.toString(n, k);
+        String[] numbers = s.split("0+");
+
+        int count = 0;
+        for (String number : numbers) {
+            long num = Long.parseLong(number);
+            if (isPrime(num)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isPrime(long num) {
+        if (num == 1) {
+            return false;
+        }
+
+        for (long i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(" "+i);
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
