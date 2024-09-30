d324b43817827eb14e1826490b33b13506e27757
smLee <sm.lee@care-biz.co.kr>
2024-09-30 22:43:36 +0900

    DoIt 알고리즘 코딩 테스트 - 기학 '선분 방향 구하기' 풀이

---
+++ DoIt/src/test/java/ddongman/doit/geometric/SegmentDirectionTest.java
@@ -0,0 +1,36 @@
+package ddongman.doit.geometric;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SegmentDirectionTest {
+
+    private SegmentDirection sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new SegmentDirection();
+    }
+
+    @Test
+    void case1() {
+        int result = sut.solution(new int[][]{{1, 1}, {5, 5}, {7, 3}});
+
+        Assertions.assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    void case2() {
+        int result = sut.solution(new int[][]{{1, 1}, {3, 3}, {5, 5}});
+
+        Assertions.assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void case3() {
+        int result = sut.solution(new int[][]{{1, 1}, {7, 3}, {5, 5}});
+
+        Assertions.assertThat(result).isEqualTo(1);
+    }
+}
