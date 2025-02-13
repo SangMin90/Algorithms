d81cdd59e973b9734be6acf088f2012e381a16d6
SangMin90 <everminelee@naver.com>
2025-02-14 00:43:50 +0900

    메서드 추출

--- DoIt/src/main/java/ddongman/doit/datastructure/ConsecutiveSumFinder.java
+++ DoIt/src/main/java/ddongman/doit/datastructure/ConsecutiveSumFinder.java
@@ -7,6 +7,19 @@ public class ConsecutiveSumFinder {
     public int solution(int n) {
         int[] prefixSum = prefixSumOf(n);
 
+        return countConsecutiveSum(n, prefixSum);
+    }
+
+    private int[] prefixSumOf(int n) {
+        int[] result = new int[n + 1];
+
+        IntStream.rangeClosed(1, n)
+                .forEach(i -> result[i] = result[i - 1] + i);
+
+        return result;
+    }
+
+    private int countConsecutiveSum(int n, int[] prefixSum) {
         int count = 0;
         int i = 0;
         int j = 1;
@@ -22,14 +35,5 @@ public class ConsecutiveSumFinder {
         }
 
         return count;
-    }
-
-    private int[] prefixSumOf(int n) {
-        int[] result = new int[n + 1];
-
-        IntStream.rangeClosed(1, n)
-                .forEach(i -> result[i] = result[i - 1] + i);
-
-        return result;
     }
 }
