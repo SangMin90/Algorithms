d516f4b4e811f939b046884e2fe054ca0fe5caa5
SangMin90 <everminelee@naver.com>
2025-02-13 23:55:05 +0900

    나머지 합 구하기

---
+++ DoIt/src/main/java/ddongman/doit/datastructure/ModSubarrayCounter.java
@@ -0,0 +1,34 @@
+package ddongman.doit.datastructure;
+
+import java.util.stream.IntStream;
+
+public class ModSubarrayCounter {
+
+    public long solution(int n, int m, int[] numbers) {
+        int[] prefixSum = prefixSumInArray(n, numbers);
+        return countPrefixSumDivisibleByM(m, prefixSum);
+    }
+
+    private long countPrefixSumDivisibleByM(int m, int[] prefixSum) {
+        return IntStream.range(1, prefixSum.length)
+                .mapToLong(i -> IntStream.range(i, prefixSum.length)
+                        .filter(
+                                j -> (prefixSum[j] - prefixSum[i - 1]) % m == 0
+                        )
+                        .count()
+                )
+                .sum();
+
+    }
+
+    private int[] prefixSumInArray(int n, int[] numbers) {
+        return IntStream.rangeClosed(0, n)
+                .map(i -> {
+                    if (i == 0) {
+                        return 0;
+                    } else {
+                        return IntStream.rangeClosed(1, i).map(k -> numbers[k - 1]).sum();
+                    }
+                }).toArray();
+    }
+}
