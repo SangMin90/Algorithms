02b2518f6e5e986d24f02f293f6681f003a38445
SangMin90 <everminelee@naver.com>
2025-02-14 00:23:10 +0900

    연속된 자연수의 합 구하기

---
+++ DoIt/src/main/java/ddongman/doit/datastructure/ConsecutiveSumFinder.java
@@ -0,0 +1,35 @@
+package ddongman.doit.datastructure;
+
+import java.util.stream.IntStream;
+
+public class ConsecutiveSumFinder {
+
+    public int solution(int n) {
+        int[] prefixSum = prefixSumOf(n);
+
+        int count = 0;
+        int i = 0;
+        int j = 1;
+        while (i <= j && j <= n) {
+            if ((prefixSum[j] - prefixSum[i]) == n) {
+                count++;
+                j++;
+            } else if (prefixSum[j] - prefixSum[i] > n) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return count;
+    }
+
+    private int[] prefixSumOf(int n) {
+        int[] result = new int[n + 1];
+
+        IntStream.rangeClosed(1, n)
+                .forEach(i -> result[i] = result[i - 1] + i);
+
+        return result;
+    }
+}
