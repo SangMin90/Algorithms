305d3735c08f30613645d00be55a23bd72379a90
SangMin90 <everminelee@naver.com>
2025-02-23 23:47:15 +0900

    DNA 비밀번호

---
+++ DoIt/src/test/java/ddongman/doit/datastructure/DNAPasswordTest.java
@@ -0,0 +1,39 @@
+package ddongman.doit.datastructure;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DNAPasswordTest {
+
+    private DNAPassword sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new DNAPassword();
+    }
+
+    @Test
+    void case1() {
+        int s = 9;
+        int p = 8;
+        String dna = "CCTGGATTG";
+        int[] passwordPolicy = new int[]{2, 0, 1, 1};
+
+        int result = sut.solution(s, p, dna, passwordPolicy);
+
+        Assertions.assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void case2() {
+        int s = 4;
+        int p = 2;
+        String dna = "GATA";
+        int[] passwordPolicy = new int[]{1, 0, 0, 1};
+
+        int result = sut.solution(s, p, dna, passwordPolicy);
+
+        Assertions.assertThat(result).isEqualTo(2);
+    }
+}
