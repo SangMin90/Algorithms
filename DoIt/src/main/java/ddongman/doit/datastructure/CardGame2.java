c8fbeadc07a6a2da85a24ad13c5bbc64b6bc9018
SangMin90 <everminelee@naver.com>
2025-02-27 22:40:12 +0900

    카드게임2

---
+++ DoIt/src/main/java/ddongman/doit/datastructure/CardGame2.java
@@ -0,0 +1,22 @@
+package ddongman.doit.datastructure;
+
+public class CardGame2 {
+    public int solution(int n) {
+        int lowerPowerOf2 = powerOf2lowerThan(n);
+        if (n % lowerPowerOf2 == 0) {
+            return n;
+        } else {
+            return 2 * (n - lowerPowerOf2);
+        }
+    }
+
+    private int powerOf2lowerThan(int n) {
+        int result = 1;
+        while (n > 1) {
+            n /= 2;
+            result *= 2;
+        }
+
+        return result;
+    }
+}
