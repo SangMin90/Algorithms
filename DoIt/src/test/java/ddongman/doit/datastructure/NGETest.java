2347db9be6cf6252b4f1183c2390028ecf2f078f
SangMin90 <everminelee@naver.com>
2025-02-27 23:04:30 +0900

    오큰수

---
+++ DoIt/src/test/java/ddongman/doit/datastructure/NGETest.java
@@ -0,0 +1,36 @@
+package ddongman.doit.datastructure;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NGETest {
+
+    private NGE sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new NGE();
+    }
+
+    @Test
+    void case1() {
+        int n = 4;
+        int[] numbers = new int[]{9, 5, 4, 8};
+
+        int[] result = sut.solution(n, numbers);
+
+        Assertions.assertThat(result).isEqualTo(new int[]{-1, 8, 8, -1});
+    }
+
+    @Test
+    void case2() {
+        int n = 4;
+        int[] numbers = new int[]{3, 5, 2, 7};
+
+        int[] result = sut.solution(n, numbers);
+
+        Assertions.assertThat(result).isEqualTo(new int[]{5, 7, 7, -1});
+    }
+
+}
