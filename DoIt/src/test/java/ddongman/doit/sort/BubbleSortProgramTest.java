018a00a1b5baab2aeac3e38c5b722dac59bd70f5
SangMin90 <everminelee@naver.com>
2025-03-04 22:14:57 +0900

    버블 소트 프로그램1

---
+++ DoIt/src/test/java/ddongman/doit/sort/BubbleSortProgramTest.java
@@ -0,0 +1,37 @@
+package ddongman.doit.sort;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BubbleSortProgramTest {
+
+    private BubbleSortProgram sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new BubbleSortProgram();
+    }
+
+    @Test
+    void case1() {
+        int n = 5;
+        int[] numbers = new int[]{10, 1, 5, 2, 3};
+
+        int result = sut.solution(n, numbers);
+
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void case2() {
+        int n = 5;
+        int[] numbers = new int[]{1, 3, 5, 7, 9};
+
+        int result = sut.solution(n, numbers);
+
+        assertThat(result).isEqualTo(1);
+    }
+}
