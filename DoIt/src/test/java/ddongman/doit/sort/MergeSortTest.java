706ac4b6a34d5ab3d47ee94e3f7c6a0ab022f01a
SangMin90 <everminelee@naver.com>
2025-03-10 23:11:46 +0900

    수 정렬하기2

---
+++ DoIt/src/test/java/ddongman/doit/sort/MergeSortTest.java
@@ -0,0 +1,27 @@
+package ddongman.doit.sort;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MergeSortTest {
+
+    private MergeSort sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new MergeSort();
+    }
+
+    @Test
+    void case1() {
+        int n = 5;
+        int[] arr = {5, 4, 3, 2, 1};
+
+        int[] result = sut.solution(n, arr);
+
+        assertThat(result).isEqualTo(new int[]{1, 2, 3, 4, 5});
+    }
+}
