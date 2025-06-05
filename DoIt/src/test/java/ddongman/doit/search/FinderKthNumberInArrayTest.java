072ae163d2e8c7e43c06a175f3d7bf4c6b1ca4fb
SangMin90 <everminelee@naver.com>
2025-06-05 12:59:46 +0900

    배열 내에서 k번째 수 찾기

---
+++ DoIt/src/test/java/ddongman/doit/search/FinderKthNumberInArrayTest.java
@@ -0,0 +1,38 @@
+package ddongman.doit.search;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class FinderKthNumberInArrayTest {
+
+    private FinderKthNumberInArray sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new FinderKthNumberInArray();
+    }
+
+    @Test
+    void case1() {
+        int n = 3;
+        int k = 7;
+
+        int result = sut.solution(n, k);
+
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    void case2() {
+        int n = 3;
+        int k = 6;
+
+        int result = sut.solution(n, k);
+
+        assertThat(result).isEqualTo(4);
+    }
+}
