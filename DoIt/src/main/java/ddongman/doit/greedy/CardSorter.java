95f28888b9e7ec61d5524b1cecef76d5b76b0990
SangMin90 <everminelee@naver.com>
2025-06-11 22:26:20 +0900

    카드 정렬하기

---
+++ DoIt/src/main/java/ddongman/doit/greedy/CardSorter.java
@@ -0,0 +1,40 @@
+package ddongman.doit.greedy;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class CardSorter {
+    public long solution(int n, int[] cards) {
+        // 카드 묶음 정렬
+        Arrays.sort(cards);
+
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i == 1) {
+                sum += (long) cards[i] * (n - 1);
+            } else {
+                sum += (long) cards[i] * (n - i);
+            }
+        }
+
+        return sum;
+    }
+
+    public long solution2(int n, int[] cards) {
+        PriorityQueue<Long> cardQueue = new PriorityQueue<>();
+
+        // 우선순위 큐에 저장
+        for (int i = 0; i < n; i++) {
+            cardQueue.add((long) cards[i]);
+        }
+
+        long sum = 0;
+        while (cardQueue.size() > 1) {
+            long bundle = cardQueue.remove() + cardQueue.remove();
+            sum += bundle;
+            cardQueue.add(bundle);
+        }
+
+        return sum;
+    }
+}
---
+++ DoIt/src/test/java/ddongman/doit/greedy/CardSorterTest.java
@@ -0,0 +1,36 @@
+package ddongman.doit.greedy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CardSorterTest {
+
+    private CardSorter sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new CardSorter();
+    }
+
+    @Test
+    void case1() {
+        int n = 3;
+        int[] cards = new int[]{10, 20, 40};
+
+        long result = sut.solution(n, cards);
+
+        assertThat(result).isEqualTo(100L);
+    }
+
+    @Test
+    void case2() {
+        int n = 3;
+        int[] cards = new int[]{10, 20, 40};
+
+        long result = sut.solution2(n, cards);
+
+        assertThat(result).isEqualTo(100L);
+    }
+}
