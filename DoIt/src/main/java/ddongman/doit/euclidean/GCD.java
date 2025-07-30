10f8592f369dc33d6741622f12cd46a29b1c9d02
SangMin90 <everminelee@naver.com>
2025-07-30 21:21:45 +0900

    최대 공약수 구하기

---
+++ DoIt/src/main/java/ddongman/doit/euclidean/GCD.java
@@ -0,0 +1,45 @@
+package ddongman.doit.euclidean;
+
+import java.util.List;
+import java.util.stream.LongStream;
+
+public class GCD {
+    public long solution(long n, long m) {
+        if (n < m) {
+            long temp = n;
+            n = m;
+            m = temp;
+        }
+
+        return convertToNum(findGcdBetween(n, m));
+    }
+
+    private long findGcdBetween(long n, long m) {
+        long r = n % m;
+        if (r == 0) {
+            return m;
+        }
+
+        List<Long> factorsOfRemain = findFactors(r);
+        return factorsOfRemain.stream().filter(factor -> m % factor == 0).max(Long::compare)
+                .orElseThrow(() -> new RuntimeException("약수가 존재하지 않습니다."));
+    }
+
+    private List<Long> findFactors(long i) {
+        return LongStream.rangeClosed(1, i)
+                .filter(num -> i % num == 0)
+                .boxed()
+                .toList();
+    }
+
+    private long convertToNum(long n) {
+        long result = 0;
+        long base = 1L;
+        while (--n >= 0) {
+            result += base;
+            base *= 10L;
+        }
+
+        return result;
+    }
+}
