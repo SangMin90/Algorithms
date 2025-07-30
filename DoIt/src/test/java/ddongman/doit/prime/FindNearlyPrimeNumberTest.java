b648dc33bc89fb8f8be60aa80b12e983691aac6f
SangMin90 <everminelee@naver.com>
2025-07-30 21:26:22 +0900

    거의 소수 구하기

---
+++ DoIt/src/test/java/ddongman/doit/prime/FindNearlyPrimeNumberTest.java
@@ -0,0 +1,45 @@
+package ddongman.doit.prime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindNearlyPrimeNumberTest {
+    private FindNearlyPrimeNumber sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new FindNearlyPrimeNumber();
+    }
+
+    @Test
+    void case1() {
+        long a = 1;
+        long b = 1000;
+
+        long result = sut.solution(a, b);
+
+        assertThat(result).isEqualTo(25L);
+    }
+
+    @Test
+    void case2() {
+        long a = 1;
+        long b = 10;
+
+        long result = sut.solution(a, b);
+
+        assertThat(result).isEqualTo(3L);
+    }
+
+    @Test
+    void case3() {
+        long a = 5324;
+        long b = 894739;
+
+        long result = sut.solution(a, b);
+
+        assertThat(result).isEqualTo(183L);
+    }
+}
