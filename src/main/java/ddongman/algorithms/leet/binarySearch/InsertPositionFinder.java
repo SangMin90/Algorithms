48c8ec01f82850ed9d0cc32c01043d86cc53b326
SangMin90 <everminelee@naver.com>
2026-06-26 19:21:11 +0900

    35. Search Insert Position

---
+++ src/main/java/ddongman/algorithms/leet/binarySearch/InsertPositionFinder.java
@@ -0,0 +1,52 @@
+package ddongman.algorithms.leet.binarySearch;
+
+public class InsertPositionFinder {
+
+    public int searchInsert(int[] nums, int target) {
+
+        return binarySearch(nums, 0, nums.length - 1, target);
+//        return binarySearchWithBottomUp(nums, target);
+    }
+
+    private int binarySearch(int[] nums, int start, int end, int target) {
+
+        if (start >= end) {
+            return nums[start] < target ? start + 1 : start;
+        }
+
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[mid] > target) {
+            return binarySearch(nums, start, mid - 1, target);
+        }
+
+        return binarySearch(nums, mid + 1, end, target);
+    }
+
+    private int binarySearchWithBottomUp(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                start = mid + 1;
+            }
+
+            if (nums[mid] > target) {
+                end = mid - 1;
+            }
+        }
+
+        return start;
+    }
+}
---
+++ src/test/java/ddongman/algorithms/leet/binarySearch/InsertPositionFinderTest.java
@@ -0,0 +1,50 @@
+package ddongman.algorithms.leet.binarySearch;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InsertPositionFinderTest {
+
+    private InsertPositionFinder sut = new InsertPositionFinder();
+
+    @Test
+    void case1() {
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+
+        int result = sut.searchInsert(nums, target);
+
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void case2() {
+        int[] nums = {1, 3, 5, 6};
+        int target = 2;
+
+        int result = sut.searchInsert(nums, target);
+
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void case3() {
+        int[] nums = {1, 3, 5, 6};
+        int target = 7;
+
+        int result = sut.searchInsert(nums, target);
+
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test
+    void case4() {
+        int[] nums = {1, 3};
+        int target = 0;
+
+        int result = sut.searchInsert(nums, target);
+
+        assertThat(result).isEqualTo(0);
+    }
+}
